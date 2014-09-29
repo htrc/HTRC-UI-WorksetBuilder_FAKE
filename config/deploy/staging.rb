d4013957ee0f84271ac40906116cebe9d3c204bd
mfall3 <mfall3@illinois.edu>
2014-09-29 12:14:24 -0500

    add

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-#role :app, %w{hathitrust@htrc6.pti.indiana.edu:9443}
+#role :app, %w{deploy@example.com}
 #role :web, %w{deploy@example.com}
 #role :db,  %w{deploy@example.com}
 
